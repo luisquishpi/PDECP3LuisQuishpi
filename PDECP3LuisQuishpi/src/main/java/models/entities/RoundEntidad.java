499080439d036fdcb3311596dac9ebed312d4c8d
Luis Quishpi <luisquishpi @138.4.205.235>
2014-10-19 02:09:40 +0200

    Actualizado entidades Ok.

--- PDECP3LuisQuishpi/src/main/java/models/entities/RoundEntidad.java
+++ PDECP3LuisQuishpi/src/main/java/models/entities/RoundEntidad.java
@@ -1,5 +1,36 @@
 package models.entities;
 
 public class RoundEntidad {
+    private int id;
 
+    private String name;
+
+    private UserEntidad userEntidad;
+
+    public RoundEntidad() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UserEntidad getUser() {
+        return userEntidad;
+    }
+
+    public void setUser(UserEntidad user) {
+        this.userEntidad = user;
+    }
 }
--- PDECP3LuisQuishpi/src/main/java/models/entities/UserEntidad.java
+++ PDECP3LuisQuishpi/src/main/java/models/entities/UserEntidad.java
@@ -1,6 +1,40 @@
 package models.entities;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class UserEntidad {
     public int id;
+
     public String name;
+
+    private Map<Integer, RoundEntidad> rounds = new HashMap<Integer, RoundEntidad>();
+
+    public UserEntidad() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return name;
+    }
+
+    public void setUsername(String username) {
+        this.name = username;
+    }
+
+    public Map<Integer, RoundEntidad> getRounds() {
+        return rounds;
+    }
+
+    public void setRounds(Map<Integer, RoundEntidad> rounds) {
+        this.rounds = rounds;
+    }
 }
